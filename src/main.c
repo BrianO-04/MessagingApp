b44dca3905c2af0eac722cf09318007bb88e07a9
Brian Olson <BrianOlson04@gmail.com>
2026-04-11 14:34:53 -0500

    Added missing new lines to main print statements

--- src/main.c
+++ src/main.c
@@ -9,20 +9,20 @@ int main(int argc, char *argv[]){
 
     if(argc == 1){
         //INIT CLIENT
-        printf("Initializing Client");
+        printf("Initializing Client\n");
         return initialize_client();
     }else if (argc == 2) {
         if(strcmp("server", argv[1]) == 0){
             //INIT SERVER
-            printf("Initializing Server");
+            printf("Initializing Server\n");
             return initialize_server();
         }else{ 
             //INIT CLIENT
-            printf("Initializing Client");
+            printf("Initializing Client\n");
             return initialize_client();
         }
     }else {
-        printf("Expected one argument!\nUsage: ./MessagingApp client OR ./MessagingApp server");
+        printf("Expected one argument!\nUsage: ./MessagingApp client OR ./MessagingApp server\n");
         return EXIT_FAILURE;
     }
 
