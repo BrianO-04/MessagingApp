c4e8b7de75e59bd85c53b9cf237ddade78f95b5a
Brian Olson <BrianOlson04@gmail.com>
2026-04-11 15:43:21 -0500

    set server_fd sockopts and bound port

--- src/server.c
+++ src/server.c
@@ -1,21 +1,43 @@
 #include "server.h"
 #include "message.h"
 
+#include <asm-generic/socket.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 
 #define PORT 8080
 int initialize_server(){
 
     int server_fd;
+    int opt = 1;
 
+    struct sockaddr_in address;
+
+    char buffer[1024] = { 0 };
+
+    // Create socket FD
     if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
         perror("Failed to initialize socket");
         exit(EXIT_FAILURE);
     }else{
         printf("Socket initialized\n");
+    }
+
+    if((setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) < 0){
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Bind socket to port
+    if(bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0){
+        perror("Failed to bind");
+        exit(EXIT_FAILURE);
     }
 
     close(server_fd);
