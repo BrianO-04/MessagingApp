633b8a8f271fffafe0e6d70c56202045e8bacfe4
Brian Olson <BrianOlson04@gmail.com>
2026-04-11 14:34:09 -0500

    Initialized server_fd socket in server.c

--- src/server.c
+++ src/server.c
@@ -1,8 +1,21 @@
 #include "server.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
 
+#define PORT 8080
 int initialize_server(){
 
-    
+    int server_fd;
 
+    if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+        perror("Failed to initialize socket");
+        exit(EXIT_FAILURE);
+    }else{
+        printf("Socket initialized");
+    }
+
+    close(server_fd);
     return 0;
 }
