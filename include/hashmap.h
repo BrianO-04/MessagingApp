173bba6b4d3be56be9478e84e826031334663531
Brian Olson <BrianOlson04@gmail.com>
2026-04-12 15:08:26 -0500

    added comments to hashmap.h

--- include/hashmap.h
+++ include/hashmap.h
@@ -6,8 +6,16 @@
 
 #define MAX_CLIENTS 64
 
+// Inserts a user into the hash table
+// Returns 1 on success
 int put(char* key, struct User* user, struct User** hash_table);
+
+// Gets a user from the hash table using the username as a key
+// Returns NULL if the user is not found
 struct User* get(char* key, struct User** hash_table);
+
+// Removes a user from the hash table and frees the user struct
+// Returns 1 on success and 0 on failure
 int delete(char* key, struct User** hash_table);
 
 #endif // HASH_H_
