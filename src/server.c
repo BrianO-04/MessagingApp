0bc6b7f8399a028374a01e3af3da91a3486662e4
Brian Olson <BrianOlson04@gmail.com>
2026-04-12 18:51:06 -0500

    Refactored server code

--- src/server.c
+++ src/server.c
@@ -27,17 +27,17 @@ struct User** users;
 
 // Mutex
 pthread_mutex_t print_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t join_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t hash_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 int main(int argc, char *argv[]){
-    char buffer[1024] = { 0 };
 
+    // Create users hash table and set base values to NULL
     users = malloc((sizeof(struct User*)) * MAX_CLIENTS);
     for(int i = 0; i < MAX_CLIENTS; i++){
         users[i] = NULL;
     }
 
-    // Create socket FD
+    // Create server socket file descriptor
     if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
         perror("Failed to initialize socket");
         exit(EXIT_FAILURE);
@@ -45,6 +45,7 @@ int main(int argc, char *argv[]){
         printf("Socket initialized\n");
     }
 
+    // Set socket options
     if((setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) < 0){
         perror("setsockopt");
         exit(EXIT_FAILURE);
@@ -59,31 +60,33 @@ int main(int argc, char *argv[]){
         exit(EXIT_FAILURE);
     }
 
-    // Start listening
+    // Start listening on socket
     if(listen(server_fd, 3) < 0){
         perror("listen");
         exit(EXIT_FAILURE);
     }
 
-    pthread_t join_thread;
+    // Create the thread that listens for incomming connections
+    pthread_t new_connections_thread;
+    pthread_create(&new_connections_thread, NULL, connection_listen, NULL);
 
-    
-    pthread_create(&join_thread, NULL, connection_listen, NULL);
+    // Wait for the connection listening thread to finish
+    pthread_join(new_connections_thread, NULL);
 
-    pthread_join(join_thread, NULL);
-
-    // Wait for all client threads to exit
+    // Wait for all client threads to finish executing before closing socket
     for(int i = 0; i < client_count; i++){
         pthread_join(client_threads[i], NULL);
     }
-
     close(server_fd);
 
+    // Clean up memory
+    // Free all usernames
     for(int i = 0; i < MAX_CLIENTS; i++){
         if(users[i] != NULL){
             free(users[i]);
         }
     }
+    // Free the users array
     free(users);
 
     return EXIT_SUCCESS;
@@ -91,45 +94,37 @@ int main(int argc, char *argv[]){
 
 void* connection_listen(void* arg){
     while(1){
-        // Accept a single connection
+        // Wait for a connection attempt
         int new_socket;
         if((new_socket = accept(server_fd, (struct sockaddr*)&address, &addrlen)) < 0){
             perror("accept");
             exit(EXIT_FAILURE);
         }
+        
+        // Get username from client
+        char namebuf[USERNAME_LEN] = { 0 };
+        ssize_t valread = read(new_socket, namebuf, USERNAME_LEN);
+        namebuf[USERNAME_LEN-1] = '\0';
+        printf("%s joined the chat\n", namebuf);
 
-        pthread_mutex_lock(&join_mutex);
-
-        pthread_t client_thread;
-
-        char* clientID = malloc(sizeof(char) * USERNAME_LEN);
-
-        // Get username
-        char nameBuffer[USERNAME_LEN] = { 0 };
-        ssize_t valread = read(new_socket, nameBuffer, USERNAME_LEN);
-        nameBuffer[USERNAME_LEN-1] = '\0';
-        printf("%s joined the chat\n", nameBuffer);
-
-        // Assign values
+        // Create User struct
         struct User* new_user = malloc(sizeof(struct User));
         new_user->username = malloc(sizeof(char) * USERNAME_LEN);
-
-        strcpy(new_user->username, nameBuffer);
-        strcpy(clientID, nameBuffer);
-
+        strcpy(new_user->username, namebuf);
         new_user->next = NULL;
         new_user->socket = new_socket;
 
-        // Insert into users array
+        // Mutual exclusion, only one thread can modify the hash table at a time  
+        pthread_mutex_lock(&hash_mutex);
+        
         put(new_user->username, new_user, users);
-
         client_count++;
 
-        fflush(stdout);
+        // Create a new thread for listening to that client's messages
+        pthread_t client_thread;
+        pthread_create(&client_thread, NULL, client_listen, new_user);
 
-        pthread_create(&client_thread, NULL, client_listen, clientID);
-
-        pthread_mutex_unlock(&join_mutex);
+        pthread_mutex_unlock(&hash_mutex);
     }
 
     pthread_exit(NULL);
@@ -137,34 +132,34 @@ void* connection_listen(void* arg){
 }
 
 void* client_listen(void* arg){
-    char client_id[USERNAME_LEN];
-    strcpy(client_id, (char*)arg);
-    free(arg);
+    // Get user struct and username from passed in arg
+    struct User* user = (struct User*)arg;
+    char* client_id = user->username;
 
-    char msgBuffer[1024] = { 0 };
-
-    int client_running = 1;
-
-    struct User* user = get(client_id, users);
-
+    // Broadcast join message to all users
     char joinMSG[USERNAME_LEN + MESSAGE_LEN];
-    snprintf(joinMSG, sizeof(joinMSG), "%s joined the chat\n", user->username);
-    send_to_all(user->username, joinMSG, sizeof(char) * (USERNAME_LEN + MESSAGE_LEN));
+    snprintf(joinMSG, sizeof(joinMSG), "%s joined the chat\n", client_id);
+    send_to_all(client_id, joinMSG, sizeof(char) * (USERNAME_LEN + MESSAGE_LEN));
 
+    // Start listening loop
+    char msgBuffer[1024] = { 0 };
+    int client_running = 1;
     while(client_running){
+
         // Read incoming message
-        // Read message text
         ssize_t valread = read(user->socket, msgBuffer, MESSAGE_LEN);
         msgBuffer[MESSAGE_LEN-1] = '\0';
 
+        // Combine Username: Message
         char final[USERNAME_LEN+MESSAGE_LEN];
         snprintf(final, sizeof(final), "%s: %s", user->username, msgBuffer);
 
-        
+        // Lock the printing mutex before printing the message
         pthread_mutex_lock(&print_mutex);
         printf("%s", final);
         pthread_mutex_unlock(&print_mutex);
 
+        // Check if the message is a valid command
         if(strcmp(msgBuffer, "/EXIT\n") == 0){ // Client Disconnect Command
             client_running = 0;
             char msg[USERNAME_LEN + MESSAGE_LEN];
@@ -191,17 +186,17 @@ void* client_listen(void* arg){
 
             send_to_ID(client_id, user_list, sizeof(char) * (USERNAME_LEN + MESSAGE_LEN));
         }
-        else{ // Normal message
+        else{ // Normal message, send to all users
             send_to_all(client_id, final, sizeof(char) * (USERNAME_LEN + MESSAGE_LEN));
         }
         
     }
 
-    pthread_mutex_lock(&join_mutex);
-
+    // If the loop has exited, the client has disconnected
+    // Lock the hash mutex and delete the user
+    pthread_mutex_lock(&hash_mutex);
     delete(client_id, users);
-
-    pthread_mutex_unlock(&join_mutex);
+    pthread_mutex_unlock(&hash_mutex);
 
     pthread_exit(NULL);
     return NULL;
