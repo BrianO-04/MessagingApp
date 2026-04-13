a315f29bc8874c35b757138634ac27e9287d3a52
Brian Olson <BrianOlson04@gmail.com>
2026-04-13 17:03:52 -0500

    Added server build support for MacOS with pthreads

--- include/server.h
+++ include/server.h
@@ -5,11 +5,21 @@
 
 int main(int argc, char *argv[]);
 
+#if defined(__APPLE__) && defined(__MACH__)
+// Listen for new clients
+void* connection_listen(void* arg);
+
+// Listen to clients
+void* client_listen(void* arg);
+#else
 // Listen for new clients
 int connection_listen(void* arg);
 
 // Listen to clients
 int client_listen(void* arg);
+#endif
+
+
 
 // Send a message to all connected clients
 void send_to_all(char* sender_id, char* msg, size_t size);
