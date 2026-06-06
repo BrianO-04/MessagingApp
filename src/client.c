0f248656ffbb18ce9ed6653e618682fbd71d0a32
Brian Olson <BrianOlson04@gmail.com>
2026-06-06 16:51:19 -0500

    Changed client_init to return a pointer to a messsage log. Added functions for sending messages and codes

--- src/client.c
+++ src/client.c
@@ -13,11 +13,16 @@ SOCKET client_fd;
 int status;
 struct sockaddr_in server_addr;
 
-int new_message;
+int* new_message;
 struct log* message_log;
 int client_active = 1;
 
-mtx_t log_lock;
+mtx_t* log_lock;
+
+struct AES_ctx enc_ctx;
+struct AES_ctx dec_ctx;
+
+char username[USERNAME_LEN];
 
 // Plain Terminal UI for testing
 int main(int argc, char *argv[]){
@@ -26,8 +31,10 @@ int main(int argc, char *argv[]){
         return EXIT_FAILURE;
     }
 
-    int status = client_init(argv[1], argv[2]);
-    if(status != 0){
+    int* new_msg = malloc(sizeof(int));
+    mtx_t* logLock = malloc(sizeof(mtx_t));
+    struct log* msg_log = client_init(argv[1], argv[2], new_msg, logLock);
+    if(msg_log == NULL){
         printf("Failed to initialize client\n");
         return -1;
     }
@@ -37,34 +44,46 @@ int main(int argc, char *argv[]){
     AES_init_ctx_iv(&aes_ctx, aes_key, iv);
 
     while(client_active){
-        mtx_lock(&log_lock);
-        if(new_message == 1){
-            AES_init_ctx_iv(&aes_ctx, aes_key, message_log->tail->iv);
+        mtx_lock(logLock);
+        if(*new_msg == 1){
+            AES_init_ctx_iv(&aes_ctx, aes_key, msg_log->tail->iv);
 
             char decrypted_msg[MESSAGE_LEN];
-            memcpy(decrypted_msg, message_log->tail->msg, MESSAGE_LEN);
+            memcpy(decrypted_msg, msg_log->tail->msg, MESSAGE_LEN);
             AES_CBC_decrypt_buffer(&aes_ctx, (uint8_t*)decrypted_msg, MESSAGE_LEN);
 
             printf("%s\n", decrypted_msg);
         }
-        new_message = 0;
-        mtx_unlock(&log_lock);
+        *new_msg = 0;
+        mtx_unlock(logLock);
     }
 
     return 0;
 }
 
-int client_init(char* uname, char* ip){
+struct log* client_init(char* uname, char* ip, int* new_msg, mtx_t* logLock){
     #if defined(_WIN32)
     // WSADATA startup required for windows sockets
     WSADATA wsa;
     if (WSAStartup(MAKEWORD(2,2), &wsa) != 0) {
         printf("WSAStartup failed\n");
-        return -1;
+        return NULL;
     }
     #endif
 
-    mtx_init(&log_lock, mtx_plain);
+    strcpy(username, uname);
+    new_message = new_msg;
+    log_lock = logLock;
+
+    // Tiny AES Setup
+    // Encryption ctx
+    uint8_t iv1[AES_BLOCKLEN] = {0};
+    AES_init_ctx_iv(&enc_ctx, aes_key, iv1);
+    // Decryption ctx
+    uint8_t iv2[AES_BLOCKLEN] = {0};
+    AES_init_ctx_iv(&dec_ctx, aes_key, iv2);
+
+    mtx_init(log_lock, mtx_plain);
 
     // Message log setup
     message_log = malloc(sizeof(struct log));
@@ -76,12 +95,12 @@ int client_init(char* uname, char* ip){
     if((client_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET){
         printf("Failed to create socket\n");
         WSACleanup();
-        return -1;
+        return NULL;
     }
     #else // POSIX systems
     if((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
         perror("Failed to create socket");
-        return -1;
+        return NULL;
     }
     #endif
 
@@ -92,13 +111,13 @@ int client_init(char* uname, char* ip){
     // Convert text address to binary
     if(inet_pton(AF_INET, ip, &server_addr.sin_addr) <= 0){
         printf("Invalid address\n");
-        return -1;
+        return NULL;
     }
 
     // Connect to the server
     if((status = connect(client_fd, (struct sockaddr*)&server_addr, sizeof(server_addr))) < 0){
         printf("Connection failed");
-        return -1;
+        return NULL;
     }
     cmd_types join_cmd = JOIN;
     send(client_fd, &join_cmd, sizeof(cmd_types), 0);
@@ -114,7 +133,7 @@ int client_init(char* uname, char* ip){
     thrd_t messaging_thread;
     thrd_create(&messaging_thread, server_listen, NULL);
     
-    return 0;
+    return message_log;
 }
 
 THRDFUNC server_listen(void* arg){
@@ -149,13 +168,38 @@ THRDFUNC server_listen(void* arg){
         char final[USERNAME_LEN+MESSAGE_LEN+2];
         snprintf(final, sizeof(final), "%s: %s", usr_buffer, decrypted_msg);
 
-        mtx_lock(&log_lock);
+        mtx_lock(log_lock);
 
         add_log(message_log, msg_buffer, usr_buffer, iv);
-        new_message = 1;
+        *new_message = 1;
 
-        mtx_unlock(&log_lock);
+        mtx_unlock(log_lock);
     }
     thrd_exit(THRDEXIT);
     return THRDEXIT;
+}
+
+void send_code(cmd_types cmd){
+    send(client_fd, &cmd, sizeof(cmd_types), 0);
+}
+
+void send_msg(char msg[MESSAGE_LEN]){
+    send_code(MESSAGE);
+    send(client_fd, enc_ctx.Iv, AES_BLOCKLEN, 0);
+
+    uint8_t iv[AES_BLOCKLEN] = { 0 };
+    memcpy(iv, enc_ctx.Iv, AES_BLOCKLEN);
+
+    // Encrypt message
+    char encrypted[MESSAGE_LEN];
+    memcpy(encrypted, msg, MESSAGE_LEN);
+    AES_CBC_encrypt_buffer(&enc_ctx, (uint8_t*)encrypted, MESSAGE_LEN);
+
+    // Send Encrypted Message
+    send(client_fd, encrypted, MESSAGE_LEN, 0);
+
+    mtx_lock(log_lock);
+    add_log(message_log, encrypted, username, iv);
+    *new_message = 1;
+    mtx_unlock(log_lock);
 }
